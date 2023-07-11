142f8faf7db741b40bd9a428f426b27c71965cda
Nick Rosen <rosennm@gmail.com>
2023-07-10 20:23:11 -0700

    add post to api

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,13 @@
 import { SignInButton, SignOutButton, useUser } from "@clerk/nextjs";
+import { log } from "console";
 import Head from "next/head";
 import { api } from "~/utils/api";
 
 export default function Home() {
-  const hello = api.example.hello.useQuery({ text: "from tRPC" });
   const user = useUser();
+
+  const { data } = api.posts.getAll.useQuery();
+  console.log({ data });
 
   return (
     <>
@@ -16,6 +19,11 @@ export default function Home() {
       <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c]">
         {!user?.isSignedIn && <SignInButton />}
         {!!user.isSignedIn && <SignOutButton />}
+        <div>
+          {data?.map((post) => (
+            <div key={post.id}>{post.content}</div>
+          ))}
+        </div>
       </main>
     </>
   );
